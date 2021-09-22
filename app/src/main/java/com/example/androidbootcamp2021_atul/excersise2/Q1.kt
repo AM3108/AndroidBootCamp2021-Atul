45926a8257248368b596502ff5091bcc74167a40
atul <atul.mishratothenew.com>
2021-09-22 19:23:59 +0530

    Initial Coomit

---
+++ app/src/main/java/com/example/androidbootcamp2021_atul/excersise2/Q1.kt
@@ -0,0 +1,34 @@
+package com.example.androidbootcamp2021_atul.excersise2
+
+class Q1 {
+
+    // Companion object make it public static
+    // So we can access it just as using ClassName.propertyName
+    companion object {
+        const val fname = "Pawan"
+        const val lname = "Shadija"
+        const val age = 25
+    }
+
+    var fname: String? = null
+    var lname: String? = null
+    var age: String? = null
+
+    // init will initialise values
+    init {
+        fname = "Atul"
+        lname = "Mishra"
+        age = "25"
+
+    }
+
+    // init will initialise details
+    // then we will print it
+    fun printInitDetails() {
+        println("Value initialised from init block")
+        println("First name: $fname")
+        println("Last name: $lname")
+        println("Age: $age")
+    }
+
+}
