9fbf84e66bc09b269318f5f279a11b6bc4fa6c98
atul <atul.mishratothenew.com>
2021-09-27 19:03:04 +0530

    Initial Commit

--- app/src/main/java/com/example/androidbootcamp2021_atul/MainActivity.kt
+++ app/src/main/java/com/example/androidbootcamp2021_atul/MainActivity.kt
@@ -1,2 +1,24 @@
 package com.example.androidbootcamp2021_atul
 
+import com.example.androidbootcamp2021_atul.excersise.Q1
+import com.example.androidbootcamp2021_atul.excersise.Q2
+
+fun main() {
+
+    // Q1 WAP using Lambda function to calculate the Simple Interest.
+    println("Q1")
+    val ob1 = Q1()
+    val principalAmt = 2500
+    val timeInYear = 2
+    val rateOfInterest = 10
+    // calling lambda function
+    ob1.simpleInterest(principalAmt, timeInYear, rateOfInterest)
+
+    // Q2 Create a list of Employee which will have name and age as properties. print the name of all employees age >30.
+    println("\nQ2")
+    println("Employees age greater than 30 are: ")
+    val ob2 = Q2()
+    ob2.createFilteredList()
+
+
+}
