3a263fc4747c753188061880c8f500ce8154475b
atul <atul.mishratothenew.com>
2021-11-12 10:48:20 +0530

    commit with Navigation

--- app/src/main/java/com/example/androidbootcamp2021_atul/MainActivity.kt
+++ app/src/main/java/com/example/androidbootcamp2021_atul/MainActivity.kt
@@ -2,50 +2,17 @@ package com.example.androidbootcamp2021_atul
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.androidbootcamp2021_atul.adapter.CustomAdapter
-import com.example.androidbootcamp2021_atul.repository.MainRepository
-import com.example.androidbootcamp2021_atul.retrofit.RetrofitService
-import com.example.androidbootcamp2021_atul.viewModel.MainViewModel
-import com.example.androidbootcamp2021_atul.viewModel.MyViewModelFactory
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
-
-    lateinit var viewModel: MainViewModel
-    private val adapter = CustomAdapter()
-    lateinit var recyclerView: RecyclerView
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
-        recyclerView = findViewById(R.id.post_details_RV)
-
-        val retrofitService = RetrofitService.getInstance()
-        val mainRepository = MainRepository(retrofitService)
-
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = adapter
-
-        viewModel = ViewModelProvider(this, MyViewModelFactory(mainRepository)).get(MainViewModel::class.java)
-
-
-        //Observing livedata
-        viewModel.dataList.observe(this, {
-            adapter.setData(it)
-        })
-
-        viewModel.errorMessage.observe(this, {
-            Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
-        })
-
-        viewModel.getAllMovies()
-
-
-
+    companion object {
+        const val MY_PREFERENCES = "login_shared_pref"
+        const val NAME_KEY = "user_full_name"
+        const val DESCRIPTION_KEY = "description_text"
     }
 }
